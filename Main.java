0b515ee8deaad44e3724f36885712a561683ad84
woczarder <wojtasessek1997@gmail.com>
2018-04-18 20:19:39 +0200

    Added empty button

---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.harddrillstudio.tutorial.javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+	    launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("LOL IM GAY");
+        Button button = new Button("I'm gay!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+        primaryStage.setScene(new Scene(root, 320, 320));
+        primaryStage.show();
+    }
+}
