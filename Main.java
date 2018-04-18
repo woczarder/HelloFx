23aa5c6f1035a2c50551ae2b0c1acb3b57ee35ff
woczarder <wojtasessek1997@gmail.com>
2018-04-18 20:23:29 +0200

    test

--- Main.java
+++ Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("LOL IM GAY");
         Button button = new Button("I'm gay!");
+        
 
         StackPane root = new StackPane();
         root.getChildren().add(button);
